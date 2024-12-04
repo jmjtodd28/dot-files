b1caf67509a5e3c2f0a3748ad326d2fbd460c8c3
Joshua Todd <jmjtodd28@gmail.com>
2024-12-04 19:55:50 +0000

    nvim and tmux dot files

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,15 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set nu")
+vim.cmd("set relativenumber")
+vim.cmd("set scrolloff=8")
+vim.cmd("set signcolumn=yes")
+
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
